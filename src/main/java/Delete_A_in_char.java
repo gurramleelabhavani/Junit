9d5d8186d0da90632c38f1ee9f7e3e6b533bc5ab
Gurram Leela Bhavani <leelabhavani353@gmail.com>
2020-03-25 22:16:31 +0530

    Initial commit

---
+++ src/main/java/Delete_A_in_char.java
@@ -0,0 +1,16 @@
+public class Delete_A_in_char {
+    public String remove_A_FromFirstTwoChars(String string)
+    {
+        String str="";
+        if(string.length()>=2 && string.charAt(0)=='A' && string .charAt(1)=='A')
+            str=string.substring(2,string.length());
+        else if(string.length()>=1 && string.charAt(0)=='A')
+            str=string.substring(1,string.length());
+        else if(string.length()>=2 && string.charAt(1)=='A')
+            str=string.charAt(0)+string.substring(2,string.length());
+        else
+            str=string;
+        return str;
+
+    }
+}
